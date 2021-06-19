e7632393567600b9234e5a36a11414994f7644b0
gocho <basabasafoo@gmail.com>
2021-06-19 19:39:54 +0900

    remove derived state

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -3,51 +3,23 @@ import { CellList } from "./cell/cell_list";
 import { Cell } from "./cell/cell";
 import { Game } from "../../game/runner/game";
 
-type PropsType = {game: Game};
-type StateType = {game: Game};
+type PropsType = {game: Game, handleOpen: (x: number, y: number) => void};
+type StateType = {};
 
 export class Body extends React.Component<PropsType, StateType> {
-  constructor(props: PropsType) {
-    super(props);
-    this.state = {
-      game: props.game
-    };
-  }
-
-  handleOpen(x: number, y: number) {
-    this
-      .state
-      .game
-      .open(x, y)
-      .then(
-        (updatedGame: Game) => {
-          this.setState({
-            game: updatedGame
-          });
-        },
-        (rejected: any) => {
-          console.info('boom(click event)');
-        }
-      );
-  }
-
   render() {
-    if (!this.state) {
-      return <></>;
-    }
-
-    const matrix = [...new Array(this.state.game.height)].map((_, y) =>
+    const matrix = [...new Array(this.props.game.height)].map((_, y) =>
       <CellList
         key={y}
-        cellList={[...new Array(this.state.game.width)].map((_, x) =>
+        cellList={[...new Array(this.props.game.width)].map((_, x) =>
           <Cell
             key={`${x}-${y}`}
             x={x}
             y={y}
-            isOpen={this.state.game.isOpen(x, y)}
-            number={this.state.game.number(x, y)}
+            isOpen={this.props.game.isOpen(x, y)}
+            number={this.props.game.number(x, y)}
             open={(ev: React.MouseEvent<HTMLElement>) => {
-              this.handleOpen(x, y);
+              this.props.handleOpen(x, y);
             }} />
         )} />
     );
--- src/components/runner.tsx
+++ src/components/runner.tsx
@@ -11,6 +11,7 @@ export class Runner extends React.Component<PropsType, StateType> {
   constructor(props: PropsType) {
     super(props);
     this.handleStart = this.handleStart.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
   }
 
   handleStart(width: number, height: number, mineCount: number) {
@@ -21,13 +22,32 @@ export class Runner extends React.Component<PropsType, StateType> {
     });
   }
 
+  handleOpen(x: number, y: number) {
+    this
+      .state
+      .game
+      .open(x, y)
+      .then(
+        (updatedGame: Game) => {
+          this.setState({
+            game: updatedGame
+          });
+        },
+        (rejected: any) => {
+          console.info('boom(click event)');
+        }
+      );
+  }
+
   render() {
     if (this.state && this.state.game) {
       return (
         <>
           <Menu handleStart={this.handleStart} />
           <hr />
-          <Body game={this.state.game} />
+          <Body
+            game={this.state.game}
+            handleOpen={this.handleOpen} />
         </>
       );
     } else {
